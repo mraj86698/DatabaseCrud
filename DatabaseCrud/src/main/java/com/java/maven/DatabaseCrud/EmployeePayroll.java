84e2302b504ee77a23573de1dac8c6804a878480
MohanRaj <mraj86698@gmail.com>
2022-09-03 20:32:58 +0530

    Added to Welcome Message for database Crud Operations

---
+++ DatabaseCrud/src/main/java/com/java/maven/DatabaseCrud/EmployeePayroll.java
@@ -0,0 +1,9 @@
+package com.java.maven.DatabaseCrud;
+
+public class EmployeePayroll
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the Database Crud Operation For Employee PayRoll" );
+    }
+}
